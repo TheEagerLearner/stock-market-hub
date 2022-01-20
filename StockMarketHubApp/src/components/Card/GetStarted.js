8373d431b5e42be7994d6f6d552b09248949166e
sanket <bandekarsanket03@gmail.com>
2022-01-21 01:20:08 +0530

    added GetStarted component

---
+++ StockMarketHubApp/src/components/Card/GetStarted.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+
+const GetStarted = () => {
+    return(
+        <View style={styles.main}>
+            <View style={styles.button}>
+                <Text style={styles.textStyle}>GET STARTED</Text>
+                <AntDesign name="right" size={30} color="black" style={styles.icon} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    main: {
+        margin: 100
+    },
+    button:{
+        width: 200,
+        height: 75,
+        backgroundColor: '#7bd4fe',
+        borderRadius: 20,
+        shadowColor:'#A5a9ff',
+        flexDirection: 'row',
+        //justifyContent: 'space-between'
+        
+    },
+
+    textStyle:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        flex: 3,
+        textAlignVertical:'center',
+        marginLeft: 20,
+        
+    },
+
+    icon:{
+        flex: 1,
+        textAlignVertical:'center',
+
+    }
+})
+
+export default GetStarted
