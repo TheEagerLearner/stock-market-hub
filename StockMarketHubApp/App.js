3d50d431b2330b1a4059420fe0d3ff2586293b21
TheEagerLearner <saurabhmul10@gmail.com>
2022-01-22 00:44:21 +0530

    Splash Screen Added

--- StockMarketHubApp/App.js
+++ StockMarketHubApp/App.js
@@ -1,16 +1,16 @@
 import React from  'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-//import SplashScreen from './src/screens/SplashScreen';
+import SplashScreen from './src/screens/SplashScreen';
 import GetStarted from './src/components/Card/GetStarted';
 
 const navigator = createStackNavigator(
   {
+    SplashScreen:SplashScreen,
     
-    Getstarted: GetStarted
   },
   {
-    initialRouteName:'Getstarted',
+    initialRouteName:'SplashScreen',
 
     defaultNavigationOptions:{
       headerShown:false
